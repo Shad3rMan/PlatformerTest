5febf0801723b299ad17380ac988d739a70cc968
Andrew Chaiko <andrewc@playtika.com>
2024-04-10 14:53:05 +0300

    controllers

---
+++ Assets/Platformer/Gameplay/PlatformerScope.cs
@@ -0,0 +1,13 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Platformer.Gameplay
+{
+	public class PlatformerScope : LifetimeScope
+	{
+		protected override void Configure(IContainerBuilder builder)
+		{
+			
+		}
+	}
+}
